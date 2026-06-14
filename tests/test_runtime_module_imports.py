2171db6c975cde93ef34ff27bf60f9aa89cdc951
Seasonal_Currency <seasonal@seasonalnet.org>
2026-06-14 04:46:07 -0400

    fix: cleanup pass 8

--- tests/test_runtime_module_imports.py
+++ tests/test_runtime_module_imports.py
@@ -1,7 +1,9 @@
 from seasonalweather.broadcast.cap_runtime import CapRuntime
 from seasonalweather.broadcast.ipaws_runtime import IpawsRuntime
+from seasonalweather.broadcast.nwws_runtime import NwwsRuntime
 
 
 def test_source_runtime_modules_importable() -> None:
     assert CapRuntime is not None
     assert IpawsRuntime is not None
+    assert NwwsRuntime is not None
